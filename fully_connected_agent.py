68b67dcb7f35cfe33b59106808423cd35194c8c3
larsXYZ <larsm.angelsen@gmail.com>
2019-03-29 12:25:34 +0100

    Our fully connected agent works! It is now the "base" agent

--- fully_connected_agent.py
+++ fully_connected_agent.py
@@ -15,14 +15,16 @@ class Agent(object):
         self.memory = deque()
         self.gamma = 0.95 #Discount rate
         self.epsilon = 1.0 #Exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
         self.model = self.create_model()
 
     #Create the model, the brain of the agent
     def create_model(self):
         model = tf.keras.models.Sequential()
-        model.add(tf.keras.layers.Dense(output_dim=10, input_dim=self.number_of_states, activation='relu'))
-        model.add(tf.keras.layers.Dense(output_dim=10, activation='relu'))
-        model.add(tf.keras.layers.Dense(output_dim=self.number_of_actions, activation='relu'))
+        model.add(tf.keras.layers.Dense(units=40, input_dim=self.number_of_states, activation='relu'))
+        model.add(tf.keras.layers.Dense(units=40, activation='relu'))
+        model.add(tf.keras.layers.Dense(units=self.number_of_actions, activation='relu'))
         model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=self.learning_rate))
 
         return model
@@ -36,11 +38,32 @@ class Agent(object):
         if random.random() < self.epsilon: #Explore
             return random.randrange(self.number_of_actions)
         else: #Greedy
-            return self.model.predict(x=state)
+            return np.argmax(self.model.predict(x=state))
 
+    def replay(self, batch_size):
 
-    def replay(self):
-        raise NotImplementedError
+        batch = random.sample(self.memory, batch_size)
+
+        for sample in batch:
+
+            state = sample[0]
+            action = sample[1]
+            reward = sample[2]
+            next_state = sample[3]
+            done = sample[4]
+
+            y = reward
+
+            if not done:
+                y = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+
+            y_pred = self.model.predict(state)
+            y_pred[0][action] = y
+
+            self.model.fit(state, y_pred, epochs=1, verbose=0)
+
+            if self.epsilon > self.epsilon_min:
+                self.epsilon *= self.epsilon_decay
 
     def load(self, filename):
         raise NotImplementedError
---
+++ test_environment.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import random
+import gym
+import numpy as np
+import tensorflow as tf
+from collections import deque
+import agent
+
+#SOURCE https://keon.io/deep-q-learning/
+EPISODES = 1000
+
+
+if __name__ == "__main__":
+    env = gym.make('CartPole-v1')
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    agent = agent.Agent(state_size, action_size)
+    # agent.load("./save/cartpole-dqn.h5")
+    done = False
+    batch_size = 32
+
+    for e in range(EPISODES):
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+        for time in range(500):
+            env.render()
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+
+            #Giving the agent more points if it stays near the center
+            reward += 0.2/( 1 + abs(state[0][0]))
+
+            #print(time, reward)
+
+            reward = reward if not done else -10
+            next_state = np.reshape(next_state, [1, state_size])
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                print("episode: {}/{}, score: {}, e: {:.2}"
+                      .format(e, EPISODES, time, agent.epsilon))
+                break
+            if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
+        # if e % 10 == 0:
+        #     agent.save("./save/cartpole-dqn.h5")
