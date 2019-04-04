d4d34028d944e9a60b34e5cbd5414edd9305c485
larsXYZ <larsm.angelsen@gmail.com>
2019-04-04 21:10:08 +0200

    Added temporal sensitivity by introducing an image buffer

--- CNN_agent.py
+++ CNN_agent.py
@@ -7,7 +7,8 @@ from collections import deque
 class Agent(object):
 
     #Initialize the agent
-    def __init__(self, number_of_states, number_of_actions):
+    def __init__(self, number_of_states, number_of_actions, buffer_size):
+        self.buffer_size = buffer_size
         self.state_size = number_of_states
         self.number_of_actions = number_of_actions
         self.learning_rate = 0.001
@@ -22,7 +23,7 @@ class Agent(object):
     def create_model(self):
 
         model = tf.keras.models.Sequential()
-        model.add(tf.keras.layers.Conv2D(filters=10, kernel_size=3, activation='relu', input_shape=(self.state_size[0],self.state_size[1],1)))
+        model.add(tf.keras.layers.Conv2D(filters=10, kernel_size=3, activation='relu', input_shape=(self.state_size[0],self.state_size[1],self.buffer_size)))
         model.add(tf.keras.layers.BatchNormalization())
         model.add(tf.keras.layers.Conv2D(filters=5, kernel_size=3, activation='relu'))
         model.add(tf.keras.layers.BatchNormalization())
@@ -42,7 +43,7 @@ class Agent(object):
     #Decide what to do depending on the current state
     def act(self, state):
 
-        state = state.reshape(1,self.state_size[0],self.state_size[1],1)
+        state = state.reshape(1,self.state_size[0],self.state_size[1],self.buffer_size)
 
         if random.random() < self.epsilon: #Explore
             return random.randrange(self.number_of_actions)
@@ -57,10 +58,10 @@ class Agent(object):
 
         for sample in batch:
 
-            state = (sample[0]).reshape(1,self.state_size[0],self.state_size[1],1)
+            state = (sample[0]).reshape(1,self.state_size[0],self.state_size[1],self.buffer_size)
             action = sample[1]
             reward = sample[2]
-            next_state = (sample[3]).reshape(1,self.state_size[0],self.state_size[1],1)
+            next_state = (sample[3]).reshape(1,self.state_size[0],self.state_size[1],self.buffer_size)
             done = sample[4]
 
 
--- spaceinv_environment.py
+++ spaceinv_environment.py
@@ -2,8 +2,37 @@ import gym
 import numpy as np
 import cv2
 import CNN_agent as agent
+import matplotlib.pyplot as plt
 
-EPISODES = 1000
+#Image buffer class
+class Image_buffer(object):
+    def __init__(self, size):
+        self.image_x = -1
+        self.image_y = -1
+        self.size = size
+        self.buffer = []
+
+    def append(self, image):
+        if len(self.buffer) == self.size:
+            self.buffer.pop(0)
+            self.append(image)
+        elif len(self.buffer) < self.size:
+            while len(self.buffer) < self.size:
+                self.image_x = np.shape(image)[0]
+                self.image_y = np.shape(image)[1]
+                self.buffer.append(image)
+        else:
+            print("BUFFER OTHER THAN EXPECTED")
+            exit(1)
+
+    def get_image_array(self):
+        if len(self.buffer) == self.size:
+            return np.array(self.buffer).reshape(1,self.image_x, self.image_y, self.size)
+        else:
+            print("BUFFER OTHER THAN EXPECTED")
+            exit(1)
+
+EPISODES = 50
 
 #Resizes the image to 84x84 and outputs a binary color image
 def preprocessing(observation):
@@ -17,35 +46,60 @@ def preprocessing(observation):
     return np.reshape(observation, (69,84,1))
 
 if __name__ == "__main__":
+
     save = input("Do you want to save your weights (y/n)\n")
+
+    #Score storage
+    score_storage = np.zeros(EPISODES)
+
+    #Image buffer, enabling the agent to achieve a sense of time
+    BUFFER_SIZE = 2
+    image_buffer = Image_buffer(size=BUFFER_SIZE)
 
     env = gym.make('SpaceInvaders-v0')
     state_size = (69,84)
     action_size = env.action_space.n
-    agent = agent.Agent(state_size, action_size)
-    agent.load("./weights/spaceinv_weights.h5")
+    agent = agent.Agent(state_size, action_size, BUFFER_SIZE)
+    #agent.load("./weights/spaceinv_weights.h5")
     done = False
     batch_size = 10
+
     for e in range(EPISODES):
         state = env.reset()
         state = preprocessing(state)
+        image_buffer.append(state)
+
         total_reward = 0
         for time in range(999999999):
 
             #env.render()
-            action = agent.act(state)
+
+            action = agent.act(image_buffer.get_image_array())
             next_state, reward, done, _ = env.step(action)
             total_reward += reward
             if time % 20 == 0: print("Episode:", e,", Frame:", time, ", Total score:",total_reward, ", Action:", action, ", Memory size:", len(agent.memory), ", Epsilon:", agent.epsilon)
 
             next_state = state = preprocessing(next_state)
-            agent.remember(state, action, reward, next_state, done)
+
+            #Recording for memories
+            state_old = image_buffer.get_image_array()
+            image_buffer.append(next_state)
+            state_new = image_buffer.get_image_array()
+
+            agent.remember(state_old, action, reward, state_new, done)
+
             state = next_state
             if done:
                 print("episode: {}/{}, score: {}, e: {:.2}"
                       .format(e, EPISODES, total_reward, agent.epsilon))
+                score_storage[e] = total_reward
                 break
             if len(agent.memory) > batch_size:
                 agent.replay(batch_size)
         if save == 'y':
             agent.save("./weights/spaceinv_weights_e" + str(e) + '.h5' )
+
+    #Saving and plotting result
+    np.save("score_storage", score_storage)
+    plt.plot( np.arange(1,EPISODES+1),score_storage)
+    plt.show()
