cfa12f9dcac1016e19050e5697f8c5e5705c5a5c
larsXYZ <larsm.angelsen@gmail.com>
2019-04-11 16:29:50 +0200

    Forgot to remove some code

--- cartpole_environment.py
+++ cartpole_environment.py
@@ -9,27 +9,6 @@ import best_agent_tracker
 #SOURCE https://keon.io/deep-q-learning/
 EPISODES = 100
 
-#Keeps track of best agent
-class Best_Agent_tracker(object):
-
-    def __init__(self):
-        self.best_agent = None
-        self.best_score = 0
-
-    #Returns stored agent
-    def get_best_agent(self):
-        return self.best_agent
-
-    #If provided agent gives better score it is chosen as best agent
-    def update_best_agent(self, agent, score):
-        if score > self.best_score or self.best_agent == None:
-
-            print("New best agent! Score:", score, "Increase:", score - self.best_score)
-
-            self.best_agent = agent
-            self.best_score = score
-
-
 if __name__ == "__main__":
 
     #Do we save the weights?
