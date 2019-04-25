4bbb2cffc8484a3a584141b333a507aff5911eee
larsXYZ <larsm.angelsen@gmail.com>
2019-04-25 21:24:03 +0200

    Added code for generating rolling average

---
+++ rolling_average.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+
+#Function implementing rolling average, for more readable plots
+def moving_average(a, n=3) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+
+
+score_storage = np.copy(np.load("cartpole_score_storage.npy"))
+EPISODES = np.shape(score_storage)[0]
+
+n = 20
+score_storage_roll_av = moving_average(score_storage,n)
+
+plt.plot(np.arange(1, EPISODES + 1), score_storage)
+plt.show()
+
+plt.plot(np.arange(1, EPISODES + 2 - n), score_storage_roll_av)
+plt.show()
